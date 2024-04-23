053d6f1c623a00f29dc75451ee067f0fbbea357e
Soufiane Lyazidi <slyazidi2002@gmail.com>
2024-04-23 22:01:36 +0200

    millores a tree

--- js/appMainPage.js
+++ js/appMainPage.js
@@ -23,6 +23,31 @@ const app = Vue.createApp({
 app.mount("#app");
 
 
+const app2 = Vue.createApp({
+    data() {
+        return {
+            mensaje1: 'Hello world',
+            mensaje2: 'Mi primer programa con',
+            jsF: 'Vue',
+            url: 'https://vuejs.org'
+        };
+    },
+    methods: {
+        mostrarUrl() {
+            return this.url;
+        },
+
+        openPage(ref) {
+            window.location.href = ref;
+            console.log();
+        }
+
+    }
+  });
+
+app2.mount("#main");
+
+
 var prevScrollpos = window.pageYOffset;
 
 /* Get the header element and it's position */
